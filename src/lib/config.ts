a2054584c1f1d7349b87548594251e02b42179e6
nekorytaylor666 <nekorytaylor2@gmail.com>
2023-08-28 14:38:02 +0600

    фы

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Roadmaps to becoming a modern developer',
-  description:
-    'Community driven roadmaps, articles and guides for developers to grow in their career.',
+  title: 'Бесплатные путеводители для разработчиков',
+  description: 'Перевод roadmap.sh на русский язык',
   url: {
     twitter: 'https://twitter.com/roadmapsh',
     youtube: 'https://youtube.com/theroadmap?sub_confirmation=1',
